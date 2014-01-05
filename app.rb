acdd2847052ab4c017b5ee4863a3fe4105c8325f
Yuichi Tateno <hotchpotch@gmail.com>
2014-01-06 00:38:01 +0900

    [WIP] impl

--- app.rb
+++ app.rb
@@ -5,6 +5,9 @@ require 'memcachier'
 require 'securerandom'
 require 'sinatra/twitter-bootstrap'
 require 'haml'
+require 'readlists-advent-calendar'
+
+
 
 class App < Sinatra::Base
   register Sinatra::Twitter::Bootstrap::Assets
@@ -14,20 +17,48 @@ class App < Sinatra::Base
   end
 
   get '/' do
-    if params[:url]
-      # validate
-      uid = SecureRandom.hex(10)
-      self.class.cache.set(uid, false)
-      EM::defer do
-        sleep 10
-        self.class.cache.set(uid, true)
+    if url = params[:url]
+      if rac = ReadlistsAdventCalendar.factory(url)
+        uid = SecureRandom.hex(10)
+        result = {
+          finished: false,
+          url: url,
+        }
+        self.class.cache.set(uid, result)
+          puts 'hoge'
+        EM::defer do
+          puts 'start!'
+          begin
+            readlists = rac.generate
+            result[:finished] = :sucesssed
+            result[:readlists] = readlists
+            self.class.cache.set(uid, result)
+          rescue
+            result[:finished] = :failed
+            self.class.cache.set(uid, result)
+          end
+        end
+        redirect "/u/#{uid}"
+      else
+        # invalid url
       end
-      redirect "/u/#{uid}"
     end
     haml :index
   end
 
   get '/u/:uid' do
-    self.class.cache.get(params[:uid]).to_s
+    result = self.class.cache.get(params[:uid])
+    if result
+      case result[:finished]
+      when :sucesssed
+        # memo: embed iframe url
+        readlists = result[:readlists]
+        result.inspect + "share-url: #{readlists.share_url}" + "public-edit-url: #{readlists.public_edit_url}"
+      when :failed
+        result.inspect
+      when false
+        'wait...'
+      end
+    end
   end
 end
---
+++ lib/readlists-advent-calendar.rb
@@ -0,0 +1,103 @@
+
+require 'readlists/anonymous'
+require 'open-uri'
+require 'nokogiri'
+
+module ReadlistsAdventCalendar
+  def factory(url)
+    [Adventar, Qiita].each do |klass|
+      if klass.const_get(:URL).match url
+        return klass.new(url)
+      end
+    end
+    nil
+  end
+  module_function :factory
+
+  class Base
+    attr_reader :url
+    def initialize(url)
+      @url = url
+    end
+
+    def html
+      @html ||= Nokogiri::HTML(open(url).read)
+    end
+
+    def generate
+      readlists = Readlists::Anonymous.create
+
+      puts "* Created anynymous readlists"
+      puts "share-url: #{readlists.share_url}"
+      puts "public-edit-url: #{readlists.public_edit_url}"
+
+      readlists.title = self.title
+      readlists.description = self.title
+
+      links.each do |url|
+        retried = false
+        begin
+          puts "Add #{url}"
+          readlists << url
+        rescue Readlists::Anonymous::RequestError => e
+          if retried
+            puts "* 2nd error.. ignore #{url}"
+          else
+            puts "* Error.. retry"
+            retried = true
+            retry
+          end
+        end
+      end
+
+      puts "* Created anynymous readlists"
+      puts "share-url: #{readlists.share_url}"
+      puts "public-edit-url: #{readlists.public_edit_url}"
+      readlists
+    end
+
+    def links; end
+
+    def title; end
+
+    def description; end
+  end
+
+  class Adventar < Base
+    URL = %r{\Ahttp://www.adventar.org/calendars/\d+}
+
+    def links
+      @links ||= html.css('a.mod-calendar-entryLink').map {|link| link.attr('href') }
+    end
+
+    def title
+      @title ||= html.css('h2')[0].inner_text.strip
+    end
+
+    def description
+      @description ||= html.css('.mod-calendarDescription')[0].inner_text.strip
+    end
+  end
+
+  class Qiita < Base
+    URL = %r{\Ahttp://qiita.com/advent-calendar/\d+/}
+
+    def links
+      unless @links
+        @links = html.css('table .user-info .content a').map {|link|
+          href = link.attr('href')
+          href[0] == '/' ? "http://qiita.com/#{href}" : href
+        }
+      end
+      @links
+    end
+
+    def title
+      @title ||= html.css('.page-title h1')[0].inner_text.strip
+    end
+
+    def description
+      @description ||= html.css('meta[name=description]')[0].attr('content')
+    end
+  end
+end
